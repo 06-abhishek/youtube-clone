e2132366b2f28b4bb5c9f03c1624fa18f6b8dbf8
Abhishek Patil <abhishekp.7841@gmail.com>
2026-03-28 11:47:17 +0530

    Updated CORS logic

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 // Import Routes
 import userRouter from "./routes/user.route.js";
@@ -8,10 +9,19 @@ import videosRouter from "./routes/video.route.js";
 
 const app = express();
 
-// Middleware
+dotenv.config();
+const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_BASE_URL];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow non-browser tools
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "CORS policy does not allow this origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
